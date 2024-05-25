1e4ee728828ce55c4729f3b4602f45889b291469
Camille <cwilcoxen4@outlook.com>
2024-05-24 19:42:26 -0700

    added calculator.js

---
+++ calculator.js
@@ -0,0 +1,14 @@
+let w = 2;
+let x = 5;
+let y = 10;
+let z = 12;
+
+console.log (y * z);
+console.log (w * x + z);
+console.log (y / x);
+console.log (z / w - x);
+console.log (w + x * y);
+console.log (w ** x);
+console.log ((w + x * y + z * x) / w);
+console.log(y - x * z + (w * x));
+console.log (z % x);
