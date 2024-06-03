94e9c3cf461c701ae9d1e7f1eba1f3a18f8556fa
Camille <158979907+camw222@users.noreply.github.com>
2024-06-02 23:08:51 -0700

    added third exercise js

---
+++ lesson04/exercise-three.js
@@ -0,0 +1,9 @@
+const cereal = ['cheerios', 'lucky charms', 'cocoa puffs', 'rice krispies', 'honeycomb'];
+console.log(cereal);
+
+for(let i=0; i < cereal.length; i++) {
+  
+  let result = [i, cereal[i]]
+  
+  console.log(result)
+}
